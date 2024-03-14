8c656ba047124822c7d76805205840625b4b8f2e
Robert Svart <robert.svart@student.nbi-handelsakademin.se>
2024-03-14 16:47:03 +0100

    Added Add User features

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,21 @@
+using Infrastructure;
+using Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApplication();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
 
 var app = builder.Build();
 
