b958456855cd57ee7a56a7ae68e035b69dd7a549
TomasLehtela <friscobrodet@gmail.com>
2024-03-20 11:42:22 +0100

    Add logic for auto increme OrderNumber - not tested

--- Domain/Models/Order/OrderModel.cs
+++ Domain/Models/Order/OrderModel.cs
@@ -7,6 +7,7 @@ namespace Domain.Models.OrderModel
     {
         [Key]
         public Guid OrderId { get; set; }
+        public int OrderNumber { get; set; }
 
         public DateTime OrderDate { get; set; }
         public decimal TotalCost { get; set; }
